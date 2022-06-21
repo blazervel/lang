60715304e5b38f5a0205c927a8f3ab93e61c0c4d
Joshua Anderton <info@joshanderton.com>
2022-06-21 13:28:50 -0700

    Include js method

--- src/Lang.php
+++ src/Lang.php
@@ -15,6 +15,36 @@ class Lang
     return trim("
       <script id=\"blazervel_lang\" type=\"text/javascript\">
         const BlazervelLang = {$js};
+
+        function lang(key, replace = {}, locale = '{{ app()->getLocale() }}', fallback = true) {
+          const { translations } = typeof BlazervelLang !== 'undefined' ? BlazervelLang : globalThis?.BlazervelLang,
+                keys = key.split('.');
+
+          let translation = null,
+              localeTranslations = translations[locale] || {}
+
+          keys.map(k => translation = localeTranslations[k] || '');
+
+          if (!translation && fallback) {
+            for (var localeKey in translations) {
+              keys.map(k => translation = translations[localeKey][k] || '');
+
+              if (translation) {
+                break;
+              };
+            };
+          };
+
+          if (translation) {
+            for (var key in replace) {
+              translation = translation.replace(':' + key, replace[key]);
+            };
+          };
+        
+          return translation || key;
+        };
+
+        window.lang = lang;
       </script>
     ");
   }
@@ -22,9 +52,7 @@ class Lang
   private function translations(): array
   {
     $translationFiles = File::files(
-      lang_path(
-        App::currentLocale()
-      )
+      lang_path()
     );
 
     $langKey = fn ($file) => (
